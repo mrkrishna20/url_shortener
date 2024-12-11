cad4db3a254e998a610599f415ac5dd05a3a96d7
Krishna <mrkrishnakumar20@gmail.com>
2024-12-11 15:47:46 +0000

    url new form functionality, validation, short_url functionality and routes added

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,23 @@
 class UrlsController < ApplicationController
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+    if @url.save
+      flash[:success] = "Short Url created: #{url_for(@url.short_url)}"
+      redirect_to :show
+    else
+      flash[:error] = "Invalid Url"
+      render :new
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:long_url)
+  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,11 @@
 class Url < ApplicationRecord
+  validates :long_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+
+  before_create :generate_short_url
+
+  private
+
+  def generate_short_url
+    self.short_url = SecureRandom.alphanumeric(6)
+  end
 end
