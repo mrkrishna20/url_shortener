0a1a47c308fdfc6cbc71e1b973f4a0306dca65dd
Krishna <mrkrishnakumar20@gmail.com>
2024-12-27 06:32:59 +0000

    encode payload code correction added

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
-    payload = { exp: exp.to_i }
+    payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
